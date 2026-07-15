3fef1e8e59f250e61a848535ec4c79f5ae93728b
avencores <64781822+AvenCores@users.noreply.github.com>
2026-07-15 15:56:51 +0300

    Обновление версии до 1.2.4 в файлах constants.py и version.txt

--- source/patcher/constants.py
+++ source/patcher/constants.py
@@ -1,7 +1,7 @@
 import re
 from packaging.version import Version
 
-VERSION = "1.2.3"
+VERSION = "1.2.4"
 MIN_AG_VERSION = "2.1.1"
 MIN_ANTIGRAVITY_VERSION = "2.3.0"
 AUTH_PATCH_SWITCH_VERSION = Version("1.23")
