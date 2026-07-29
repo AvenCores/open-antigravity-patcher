37094d7b195737ddc99ad6eb3829a081a5dd9fd1
avencores <64781822+AvenCores@users.noreply.github.com>
2026-07-29 10:20:49 +0300

    V1.2.9

--- source/patcher/constants.py
+++ source/patcher/constants.py
@@ -1,6 +1,6 @@
-VERSION = "1.2.8"
+VERSION = "1.2.9"
 MIN_AG_VERSION = "2.1.1"
-MIN_ANTIGRAVITY_VERSION = "2.3.1"
+MIN_ANTIGRAVITY_VERSION = "2.4.3"
 DOWNLOAD_URL = "https://antigravity.google/download"
 
 
